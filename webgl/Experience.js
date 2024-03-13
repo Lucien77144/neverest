5053454c62d7634c9f4b67b70d02f6fe3c952bbf
Jules Mesnil <jules.mesnil@gmail.com>
2024-03-13 14:05:51 +0100

    debug: added drag option

--- webgl/Experience.js
+++ webgl/Experience.js
@@ -5,6 +5,10 @@ import Sizes from './Utils/Sizes'
 import Resources from './Utils/Resources'
 import Stats from './Utils/Stats'
 import SceneManager from './Utils/SceneManager'
+import { SingletonManager } from '~/vendor/singleton'
+import Cursor from './Utils/Cursor'
+import Viewport from '~/utils/Viewport'
+import DragManager from '~/Utils/DragManager'
 import ScrollManager from './Utils/ScrollManager'
 
 export default class Experience {
@@ -32,10 +36,13 @@ export default class Experience {
     this.debug = null
     this.stats = null
     this.scrollManager = null
+    this.dragManager = null
     this.sceneManager = null
     this.renderer = null
     this.time = null
     this.resources = null
+    this.pane = null
+    this.offset = { x: 0, y: 0 }
 
     // Init
     this.init()
@@ -70,6 +77,42 @@ export default class Experience {
     this.config.height = boundings.height || window.innerHeight
   }
 
+
+  /**
+   * Drag Events
+   */
+  setDrag() {
+    this.pane.addBlade({ view: 'separator' })
+
+    const folder = this.pane.addFolder({ title: 'Position', expanded: false })
+
+    this.pane.dragButton = folder.addButton({ title: 'Drag Position' })
+
+    folder.addButton({ title: 'Reset Position' }).on('click', () => {
+      this.handlePosChange({ x: 0, y: 0 })
+    })
+
+    this.pane.addBlade({ view: 'separator' })
+  }
+
+  onDrag (e) {
+    this.handlePosChange({ x: this.offset.x - e.delta.x, y: this.offset.y - e.delta.y })
+  }
+
+  handlePosChange ({ x, y }) {
+    this.offset.x = x
+    this.offset.y = y
+    // this.pane.style.transform = `translate(${this.offset.x}px, ${this.offset.y}px)`
+    document.querySelector('.tp-dfwv').style.transform = `translate(${this.offset.x}px, ${this.offset.y}px)`
+  }
+
+  setEvents() {
+    this.handleDrag = this.onDrag.bind(this)
+
+    this.dragManager = new DragManager({ el: this.pane.dragButton.element })
+    this.dragManager.addEventListener('drag', this.handleDrag)
+  }
+
   /**
    * Get debug
    */
@@ -77,13 +120,16 @@ export default class Experience {
     if (!this.config.debug) return
 
     const { landing, toggleLanding } = useDebugStore()
-    const pane = new Pane({
+    this.pane = new Pane({
       title: 'Debug',
       expanded: true,
     })
-    pane.addBinding({ landing }, 'landing').on('change', () => toggleLanding())
+    this.pane.addBinding({ landing }, 'landing').on('change', () => toggleLanding())
 
-    return pane
+    this.setDrag()
+    this.setEvents()
+
+    return this.pane
   }
 
   /**
