a495ec11bac8c8e8d73bee497077c7360a8d7beb
Louis Cuenot <70881127+LouisCuenot@users.noreply.github.com>
2024-04-11 09:34:55 +0200

    feat:AppearingInfo

--- webgl/Components/Intro/InfoLine/InfoLine.js
+++ webgl/Components/Intro/InfoLine/InfoLine.js
@@ -5,6 +5,7 @@ import {
   LineBasicMaterial,
   Mesh,
   MeshNormalMaterial,
+  Object3D,
   RawShaderMaterial,
   ShaderMaterial,
   Uniform,
@@ -15,6 +16,7 @@ import InfoLineVert from './InfoLineShader/InfoLineShader.vert?raw'
 import InfoLineFrag from './InfoLineShader/InfoLineShader.frag?raw'
 import Experience from '~/webgl/Experience'
 import { TextGeometry } from 'three/addons/geometries/TextGeometry.js'
+import gsap from 'gsap'
 
 export default class InfoLine extends BasicItem {
   constructor(points, inputText) {
@@ -22,6 +24,7 @@ export default class InfoLine extends BasicItem {
 
     // New elements
 
+    this.rootObj = null
     this.lineGeometry = null
     this.lineMaterial = null
     this.line = null
@@ -30,9 +33,15 @@ export default class InfoLine extends BasicItem {
     this.text = null
     this.points = points
     this.inputText = inputText
+    this.isVisible = Math.sin(this.points[0].z) > 0
 
     // Init
     this.init()
+  }
+
+  setRootGeometry(){
+    this.rootObj = new Object3D()
+    this.rootObj.position.set(this.points[0].x,this.points[0].y,this.points[0].z)
   }
 
   setLineGeometry() {
@@ -49,7 +58,7 @@ export default class InfoLine extends BasicItem {
         ),
         uLowestpoint: new Uniform(this.points[0].y),
         uHighestPoint: new Uniform(this.points[this.points.length - 1].y),
-        uProgress: new Uniform(0.9),
+        uProgress: new Uniform(this.isVisible ? 1: 0),
       },
       transparent: true,
     })
@@ -63,7 +72,7 @@ export default class InfoLine extends BasicItem {
     this.textGeometry = new TextGeometry(this.inputText,{
       font: this.experience.resources.items.roboto,
       size: 1,
-      depth: 10,
+      depth: 1,
       curveSegments: 1,
     })
 
@@ -80,11 +89,21 @@ export default class InfoLine extends BasicItem {
 
   setGroup() {
     this.item = new Group()
+    this.item.add(this.rootObj)
     this.item.add(this.line)
     //this.item.add(this.text)
   }
 
+  changeVisibility(visibility){
+    this.isVisible = visibility 
+    gsap.to(this.lineMaterial.uniforms.uProgress, {
+      value:visibility ? 1 : 0,
+      duration:0.6
+    })
+  }
+
   init() {
+    this.setRootGeometry()
     this.setLineGeometry()
     this.setLineMaterial()
     this.setLine()
@@ -95,7 +114,21 @@ export default class InfoLine extends BasicItem {
   }
 
   update() {
-    this.lineMaterial.uniforms.uProgress.value =
-      Math.sin(this.experience.time.current * 0.002) * 0.5 + 0.5
+    
+
+    const rootObjPosition = new Vector3()
+    this.rootObj.getWorldPosition(rootObjPosition)
+
+    
+    if(this.isVisible && Math.sin(rootObjPosition.normalize().z) < 0){
+      this.changeVisibility(false)
+    }
+    if(!this.isVisible && Math.sin(rootObjPosition.normalize().z) > 0){
+      this.changeVisibility(true)
+    }
+    
+
+     
+    
   }
 }
