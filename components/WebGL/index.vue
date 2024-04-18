c7ffc1c92d59be1dd0c45c0e6238a66f0f596758
Jules Mesnil <jules.mesnil@gmail.com>
2024-04-18 17:29:19 +0200

    add start button

--- components/WebGL/index.vue
+++ components/WebGL/index.vue
@@ -2,11 +2,17 @@
   <canvas ref="canvas" class="exp" />
   <UIInterestData />
   <UITitle />
+  <div ref="startBtn" class="startBtn">
+    <UIBtn @click="start()">{{ $t('START') }}</UIBtn>
+  </div>
 </template>
 
 <script setup lang="ts">
 import gsap from 'gsap'
 import Experience from '~/webgl/Experience'
+
+// Scene
+import scenes from '~/const/scenes.const'
 
 // Plugins
 const { $bus }: any = useNuxtApp()
@@ -17,9 +23,45 @@ const exp = shallowRef<Experience | null>(null)
 // Refs
 const loadValue = ref<number>(0)
 const canvas = ref<HTMLElement | null>(null)
+const startBtn = ref<HTMLElement | null>(null)
 
 // Route
 const route = useRoute()
+
+// get active scene
+const scene = computed(() => useNavigationStore().getScene)
+
+watch(scene, (v) => {
+  console.log(v)
+
+  if (v?.name == 'intro') {
+    gsap.to(startBtn.value, {
+      duration: 0,
+      y: 0,
+      opacity: 1,
+      ease: 'power2.inOut',
+      onComplete: () => {
+        startBtn.value?.classList.add('active')
+      },
+    })
+  } else {    
+    gsap.to(startBtn.value, {
+      duration: 1,
+      y: 100,
+      opacity: 0,
+      ease: 'power2.inOut',
+      onComplete: () => {
+        startBtn.value?.classList.remove('active')
+      },
+    })
+  }
+})
+
+function start() {
+  const scene = scenes.nav.list.find((s) => s.name === 'basecamp')
+
+  scene && $bus.emit('scene:switch', scene)
+}
 
 // On component mounted, create the experience
 onMounted(() => {
