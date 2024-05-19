7746a4815158e146ae505aff65bd5dc7eaa930e6
Lucien <lucien.boisseau-sable@smice.com>
2024-05-19 18:23:18 +0200

    fix(subtitles): style and disable

--- components/_UI/Subtitle/index.vue
+++ components/_UI/Subtitle/index.vue
@@ -1,14 +1,19 @@
 <template>
-  <div v-if="subtitles?.length > 0" class="subtitles">
-    <div class="subtitles__wrapper">
-      <div v-for="s in subtitles" class="subtitles__item">
-        {{ s.text }}
+  <div v-if="subtitles?.length > 0 && !disabled" class="subtitles">
+    <div class="subtitles__container">
+      <div class="subtitles__wrapper">
+        <div v-for="s in subtitles" class="subtitles__item">
+          {{ s.text }}
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+// Store
+const disabled = computed(() => useSubtitlesStore().getDisabled)
+
 // Cues
 const subtitles = computed(() =>
   Object.values({ ...useSubtitlesStore().getCues })
--- stores/subtitles.store.ts
+++ stores/subtitles.store.ts
@@ -1,19 +1,27 @@
 type TSubtitle = {
   cues: VTTCue[]
+  disabled: boolean
 }
 
 export const useSubtitlesStore = defineStore('subtitles', {
   state: (): TSubtitle => ({
     cues: [],
+    disabled: false,
   }),
   getters: {
     getCues(): TSubtitle['cues'] {
       return this.cues
     },
+    getDisabled(): TSubtitle['disabled'] {
+      return this.disabled
+    },
   },
   actions: {
     setCues(val: TSubtitle['cues']) {
-      this.cues = val
+      this.cues = { ...val }
+    },
+    setDisabled(val: TSubtitle['disabled']) {
+      this.disabled = val
     },
   },
 })
