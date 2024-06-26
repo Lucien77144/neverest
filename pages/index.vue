b82e0ba66fb56fc611c29b7b3e4e9f86c0920438
Lucien <lucien.boisseau-sable@smice.com>
2024-06-26 23:13:07 +0200

    feat device support

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,19 @@
 <template>
-  <WebGL />
+  <WebGL v-if="isAllowed" />
+  <div v-if="!isAllowed">
+    $t('NOT_SUPPORTED')
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const isAllowed = ref(false)
+
+onMounted(() => {
+  const viewport = new Viewport()
+  isAllowed.value = viewport.width > 920
+
+  onUnmounted(() => {
+    viewport.destroy()
+  })
+})
+</script>
